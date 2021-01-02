41ab598f3a77b562c621705be3f45ab6ea452f41
eduardogb <eduardo.garciab@hotmail.com>
2021-01-02 17:49:59 -0600

    [ADD] Assets, layout and structure initial

---
+++ src/layout/Layout.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const MyComponent = () => {
+    return (
+        <>
+            <header>
+            </header>
+            <main>
+
+                <section>
+                </section>
+
+                <section>
+                </section>
+
+                <section>
+                </section>
+
+            </main>
+            <footer>
+
+            </footer>
+        </>
+    );
+};
+
+export default MyComponent;
